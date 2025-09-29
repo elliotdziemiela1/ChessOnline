6932d3c5d9184d3f9fece387cc3bdc1d7e9ad249
Your Name <elliotanthony9@gmail.com>
2025-09-29 15:07:31 -0500

    finished intial boilerplate server/client socket code

--- client.cpp
+++ client.cpp
@@ -29,6 +29,7 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
+    // Creating Socket
     SOCKET connectSocket = INVALID_SOCKET;
 
     connectSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
@@ -38,5 +39,56 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
+    // Connect to server
+    iResult = connect(connectSocket, ptr->ai_addr, ptr->ai_addrlen);
+    if (iResult != 0) {
+        printf("connect() error: %d\n", iResult);
+        closesocket(connectSocket);
+        connectSocket = INVALID_SOCKET;
+    } 
+
+    freeaddrinfo(result);
+
+    if (connectSocket == INVALID_SOCKET){
+        printf("Unable to connect to server.\n");
+        WSACleanup();
+        return 1;
+    }
+
+    // send data to server
+    const char * sendbuf = "test buffer";
+    iResult = send(connectSocket, sendbuf, (int)sizeof(sendbuf), 0);
+    if (iResult == SOCKET_ERROR){
+        printf("send() error: %d\n", WSAGetLastError());
+        closesocket(connectSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    // shutdown sending portion of socket
+    iResult = shutdown(connectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown sending error: %d\n", WSAGetLastError());
+        closesocket(connectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // recieve data from server
+    char recvbuf[DEFAULT_BUFLEN];
+    do {
+        iResult = recv(connectSocket, recvbuf, DEFAULT_BUFLEN, 0);
+        if (iResult > 0){
+            printf("bytes received: %d\n", iResult);
+        } else if (iResult == 0) {
+            printf("connection to server has closed");
+        } else {
+            printf("recv error: %d\n", WSAGetLastError());
+        }
+    } while (iResult > 0);
+
+    closesocket(connectSocket);
+    WSACleanup();
+    
     return 0;
 }
